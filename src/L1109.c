8ea2b3df031d6053007792c0e53dfa47b13c2f3d
Edward_163 <839629720@qq.com>
2022-07-17 22:36:47 +0800

    home

---
+++ src/L1109.c
@@ -0,0 +1,31 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <uthash.h>
+
+/*
+先做L1094,再来这
+ 这两道题目理解下啥是差分
+
+ */
+/// 思考:为啥此题换了个描述,我就想不出来怎么套用差分?!!
+int* corpFlightBookings(int** bookings, int bookingsSize, int* bookingsColSize, int n, int* returnSize){
+//    int* ans=malloc(sizeof(int)*n); // overtime
+//    memset(ans,0,sizeof(int)*n);
+//    for(int a=0;a<bookingsSize;a++){
+//        for(int b=bookings[a][0]-1;b<=bookings[a][1]-1;b++){
+//            ans[b]+=bookings[a][2];
+//        }
+//    }
+//    *returnSize=n;
+//    return ans;
+}
+int main() {
+    // @formatter:off
+    
+    return 0;
+    // @formatter:on
+}
