d519e7f9ccc5d1983906a7309831fe9230c61c10
YuJun Huang <huangyujun6@huawei.com>
2022-10-04 09:50:12 +0800

    comp 备考科目2

---
+++ src/study_swap_charArr.c
@@ -0,0 +1,39 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+/*
+ 
+ */
+void swap(char* s1,char* s2){
+    char* tmp=s1;
+    s1=s2;
+    s2=tmp;
+}
+void swap2(char** s1,char** s2){
+    char *tmp=*s1;
+    *s1=*s2;
+    *s2=tmp;
+}
+void swap3(char* s1,char* s2){
+    char *tmp=s1;
+    *s1=s2;
+    *s2=*tmp;
+}
+int main()
+{
+    // @formatter:off
+    char s1[]="one";
+    char s2[]="two"; // (gdb) p &s1 $2 = (char (*)[4]) 0xc8e87ff64c(gdb) p &s2 $3 = (char (*)[4]) 0xc8e87ff648
+    /// 目前网上海尔解决办法是,开辟新空间,利用strcpy交换
+    // swap(s1,s2);
+    // swap2(s1,s2);
+    // swap2(&s1,&s2);
+    swap3(&s1,&s2);
+    printf("%s  \n %s",s1,s2);fflush(stdout);
+    return 0;
+    // @formatter:on
+}
