8aaa0627df41f85d6353a4258efa28e41a0dd405
YuJun Huang <huangyujun6@huawei.com>
2022-10-15 09:57:08 +0800

    comp 备考科目2

--- src/study_test.c
+++ src/study_test.c
@@ -31,11 +31,55 @@ void test02(){
         printf("小于2\n");fflush(stdout);
     }
 }
+#pragma pack(1)
+struct s0{
+    char c1;
+    char c2;
+    int a;
+};
+struct s1{
+    char c1;
+    int a;
+    char c2;
+};
+struct s2{
+    char c1[5];
+    int i;
+    short s;
+};
+struct s3{
+    char c1[5]; // 假设从0x0000到0x0004地址全被占用,因为有int,按4字节对齐,留白3字节(地址0x0005开始)
+    short s; // short从哪里开始分配??0x0005还是0x0006开始??因为short地址必须是2的倍数,所以从0x0006开始,留白1字节(地址0x0005)
+    int i;
+};
+void test03(){
+    printf("s0大小是%d \n",sizeof(struct s0));fflush(stdout);
+    printf("s1大小是%d \n",sizeof(struct s1));fflush(stdout);
+    printf("s2大小是%d \n",sizeof(struct s2));fflush(stdout);
+    printf("s3大小是%d \n",sizeof(struct s3));fflush(stdout);
+}
+void test04(){
+    #pragma pack(8)
+    struct A {
+        short a;
+        int b;
+    };
+    struct B{
+        char e[2];
+        int f;
+        short g;
+        struct A j;
+        double i;
+    };
+    printf("B大小是%d \n",sizeof(struct B));fflush(stdout);
+}
 int main()
 {
     // @formatter:off
     // test01();
-    test02();
+    // test02();
+    // test03();
+    test04();
     return 0;
     // @formatter:on
 }
