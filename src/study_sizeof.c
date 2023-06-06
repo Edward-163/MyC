917ae5104dba4465008d55c45884eb66155352d5
Ed-1或者163邮 <53593314+Edward-163@users.noreply.github.com>
2023-06-06 19:14:44 +0800

    Create study_sizeof.c

---
+++ src/study_sizeof.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include "uthash.h"
+#include "securec.h"
+
+/*
+ 
+ */
+
+int main()
+{
+    // @formatter:off
+    char arr[]="ab";
+    printf("%d \n",sizeof(arr));fflush(stdout); // 3
+    return 0;
+    // @formatter:on
+}
