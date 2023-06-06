f909cfe17b3f9514b369a6d615d57f93f40d3796
Ed-1或者163邮 <53593314+Edward-163@users.noreply.github.com>
2023-06-06 19:32:54 +0800

    Update BigEndian.c

--- src/BigEndian.c
+++ src/BigEndian.c
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+
 /***
     基础知识 32位 32位地址线
     无符号int ox12345678 int4字节,32位
 
-    知识点:字节序
-    低地址     高地址
-    优先介绍大字节序,如 ox 12 34 56 78 赋值给32位机器上的int a
+    大字节序
+    低地址(高位字节)     高地址
+    int a = 0x 12 34 56 78 赋值给32位机器上的int a
     int a = 00000001 00000010 ...剩余6端省略
     转换成16进制表示
     int a = 0x 1 2 3 4 5 6 7 8
@@ -30,10 +31,31 @@
  */
 
 
+void test()
+{
+    /// 小端环境,输入123 456 789
+    char s[100] = {0x0};
+    int i = 0;
+    int c = 0xa1a2a3a4;
+    scanf("%c", &c); // 字符对应的asic码(10进制)   '0'==48 'A'==65 'a'==97
+    scanf("%d", &i);
+    scanf("%s", s);
+    printf("%c,%d,%s,0x%x\n", c, i, s, c);
+}
 
+int main()
+{
+    test();
 
-int main() {
-
+    int i = 0x12345678;
+    char c = (char) i;
+    /// 指针变量存的是地址,那么就应该把i的地址赋值给p
+    char *p = (char *) &i;
+    /// 下面也侧面说明了,截断是直接舍去高位地址,保留低位地址
+    printf("%x \n", c);
+    fflush(stdout); // 打印12是大端,打印78是小端(本机)
+    printf("%x \n", *p);
+    fflush(stdout); // 打印12是大端,打印78是小端
     return 0;
 }
 
