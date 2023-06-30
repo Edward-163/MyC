58926bd118122c4cd2d3677ead38970c169bd9c0
Ed-1或者163邮 <53593314+Edward-163@users.noreply.github.com>
2023-06-30 17:00:49 +0800

    Update study_sprintf_atoi.c

--- src/study_sprintf_atoi.c
+++ src/study_sprintf_atoi.c
@@ -6,34 +6,44 @@
 #include <stdbool.h>
 #include "securec.h"
 
-/*
- 
- */
-
-int main()
+void ru_men()
 {
-    // @formatter:off
+    printf("%s \n","sprintf------------");fflush(stdout);
+    /*
+     sprintf(char* destBuf,const char* format,...) 把...数据按照format格式化后塞入destBuf中,注意: 会追加\0
+     */
     char dstBuf[9];
-    int val=16;
-    /// 参3参4... 按照参2格式化str转化后 塞入参1中     注意: 会追加\0
     sprintf(dstBuf,"%d",12);
     printf("%s \n",dstBuf );fflush(stdout);
     sprintf(dstBuf,"%d@%d",10,9);
     printf("%s \n",dstBuf );fflush(stdout);
+}
+void secure(){
+    printf("%s \n","sprintf_s------------");fflush(stdout);
+    /*
+     sprint_s(char* destBuf,size_t destMax,const char* format,...) 注意destMax包含\0
+        succ返回写入字符数,
+        fail返回-1
+     */
+    char dstBuf[9];
+    /// snprintf_s新增参2:dstBufMax 参3变更,不包含结尾\0
+    printf("写入字符数=%d \n",snprintf_s(dstBuf,sizeof(dstBuf),2,"%d",123));fflush(stdout);
+    printf("destBuf=%s \n",dstBuf );fflush(stdout);
 
     /// 新增参2: 理论值为参1的字节大小,参2不能随便给个intMax,会造成缓冲区溢出       注意: 会追加\0
-    sprintf_s(dstBuf,sizeof(dstBuf),"%d",123);
-    printf("sprintf_s: %s \n",dstBuf );fflush(stdout);
+    printf("写入字符数=%d \n",sprintf_s(dstBuf,sizeof(dstBuf),"%d",123));fflush(stdout);
+    printf("destBuf=%s \n",dstBuf );fflush(stdout);
 
     /// 参2:拷贝的字节数,如果格式化后的字符串长度大于等于 size，超过 size 的部分会被截断，只将其中的 (size-1) 个字符复制到 str 中，并给其后添加一个字符串结束符 \0，返回值为欲写入的字符串长度。
-    snprintf(dstBuf,2,"%d",123);
-    printf("%s \n",dstBuf );fflush(stdout);
+    printf("写入字符数=%d \n",snprintf(dstBuf,2,"%d",123));fflush(stdout);
+    printf("destBuf=%s \n",dstBuf );fflush(stdout);
 
-    /// snprintf_s新增参2:dstBufMax 参3变更,不包含结尾\0
-    snprintf_s(dstBuf,sizeof(dstBuf),2,"%d",123);
-    printf("%s \n",dstBuf );fflush(stdout);
-
-    // fseek()
+}
+int main()
+{
+    // @formatter:off
+    ru_men();
+    secure();
 
     /// 首字母不是数组会直接返0  实践证明:"尽可能多地"解析出数字,一旦出现非数字字符就中断解析
     int i = atoi("a50asdfasdfasd9");
