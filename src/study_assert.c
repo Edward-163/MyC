af501f839c99f1846917444af52bad92270758c9
Ed-1或者163邮 <53593314+Edward-163@users.noreply.github.com>
2023-07-03 11:39:32 +0800

    Update study_assert.c

--- src/study_assert.c
+++ src/study_assert.c
@@ -4,19 +4,22 @@
 #include <math.h>
 #include <ctype.h>
 #include <stdbool.h>
+// 可以通过在包含#include <assert.h>的语句之前插入 #define NDEBUG 来禁用assert调用
+#define NDEBUG
 #include <assert.h>
 
 /*
  
  */
-
 int main()
 {
     // @formatter:off
+    assert(0); // 实际运行时报错
     assert(1>2); // 实际运行时报错
     // assert(3>2); // 实际运行时正常
-    
-    _Static_assert(2+2==4,"666");
+    printf("%s \n","test nodebug assert");fflush(stdout);
+
+    _Static_assert(2+2==4,"err info");
     // _Static_assert(2+2==3,"666"); // 编译失败
     // static_assert(2+2==3,"666"); // 编译失败,include assert.h之后,此行等价与上行
     // static_assert(2+2==4,"666");
